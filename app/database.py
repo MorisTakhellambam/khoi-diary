dcb38400382fcd1dcd15299b23b111184b5b82fd
MorisTakhellambam <89761373+MorisTakhellambam@users.noreply.github.com>
2025-11-01 09:44:31 +0530

    added databases

---
+++ app/database.py
@@ -0,0 +1,9 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./notes.db")
+
+engine = create_engine(DATABASE_URL, connect_args={})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
