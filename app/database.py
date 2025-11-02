38a284ff457edcc639f207359563120c53eadec0
MorisTakhellambam <89761373+MorisTakhellambam@users.noreply.github.com>
2025-11-02 22:43:05 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,9 +1,22 @@
+# app/database.py
 import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./notes.db")
+load_dotenv()
 
-engine = create_engine(DATABASE_URL, connect_args={})
+DATABASE_URL = "postgresql://neondb_owner:npg_RJukx4IeG0pw@ep-proud-pine-a1hbfor8-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+
+# Ensure it’s Postgres, not SQLite
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL not found in environment variables")
+
+# Optional: force SSL mode (Neon requires it)
+if "sslmode" not in DATABASE_URL:
+    DATABASE_URL += "?sslmode=require"
+
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
